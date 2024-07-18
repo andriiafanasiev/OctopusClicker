a1d62e42fcc52c4d3a8fc132efa2616a9c9bd022
andriiafanasiev <andreynoobroo@gmail.com>
2024-07-18 16:57:56 +0200

    Added energy, and energy recovery

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 const $score = document.querySelector(".game__score");
 const $circle = document.querySelector(".game__clicker-circle");
 const $mainImg = document.querySelector(".game__main-image");
+const $energie = document.querySelector(".energie__value");
 
 start();
 
 function start() {
   setScore(getScore());
+  setEnergie(getEnergie());
   setImage();
 }
 
@@ -22,42 +24,61 @@ function setScore(score) {
   localStorage.setItem("score", score);
   $score.textContent = score;
 }
+// Image change
 function setImage() {
   if (getScore() > 600) {
     $mainImg.setAttribute("src", "/img/octopus/normal.png");
   }
 }
+// Energie control
+
+function getEnergie() {
+  return Number(localStorage.getItem("energie")) ?? 1000;
+}
+function setEnergie(energie) {
+  localStorage.setItem("energie", energie);
+  $energie.textContent = energie;
+}
+
+setInterval(() => {
+  setEnergie(getEnergie() + 1);
+}, 2000);
+
+// Click event
 
 $circle.addEventListener("click", (event) => {
-  const rect = $circle.getBoundingClientRect();
+  if (getEnergie() > 0) {
+    const rect = $circle.getBoundingClientRect();
 
-  const offfsetX = event.clientX - rect.left - rect.width / 2;
-  const offfsetY = event.clientY - rect.top - rect.height / 2;
+    const offfsetX = event.clientX - rect.left - rect.width / 2;
+    const offfsetY = event.clientY - rect.top - rect.height / 2;
 
-  const DEG = 40;
+    const DEG = 40;
 
-  const tiltX = (offfsetY / rect.height) * DEG;
-  const tiltY = (offfsetX / rect.width) * -DEG;
+    const tiltX = (offfsetY / rect.height) * DEG;
+    const tiltY = (offfsetX / rect.width) * -DEG;
 
-  $circle.style.setProperty("--tiltX", `${tiltX}deg`);
-  $circle.style.setProperty("--tiltY", `${tiltY}deg`);
+    $circle.style.setProperty("--tiltX", `${tiltX}deg`);
+    $circle.style.setProperty("--tiltY", `${tiltY}deg`);
 
-  setTimeout(() => {
-    $circle.style.setProperty("--tiltX", `0deg`);
-    $circle.style.setProperty("--tiltY", `0deg`);
-  }, 300);
+    setTimeout(() => {
+      $circle.style.setProperty("--tiltX", `0deg`);
+      $circle.style.setProperty("--tiltY", `0deg`);
+    }, 300);
 
-  const plusOne = document.createElement("div");
-  plusOne.classList.add("plus-one");
-  plusOne.textContent = "+1";
-  plusOne.style.left = `${event.clientX - rect.left}px`;
-  plusOne.style.top = `${event.clientY - rect.top}px`;
+    const plusOne = document.createElement("div");
+    plusOne.classList.add("plus-one");
+    plusOne.textContent = "+1";
+    plusOne.style.left = `${event.clientX - rect.left}px`;
+    plusOne.style.top = `${event.clientY - rect.top}px`;
 
-  $circle.parentElement.appendChild(plusOne);
+    $circle.parentElement.appendChild(plusOne);
 
-  addOne();
+    addOne();
+    setEnergie(getEnergie() - 1);
 
-  setTimeout(() => {
-    plusOne.remove();
-  }, 2000);
+    setTimeout(() => {
+      plusOne.remove();
+    }, 2000);
+  }
 });
